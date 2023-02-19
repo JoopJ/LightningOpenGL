75fc6056b5e0a9aeeb7d878926db41354ce91248
JoopJ <joppenheimerjo@gmail.com>
2023-02-19 17:44:20 +0000

    Added ImGui windows
    Added ImGui, one window for Lightning pattern generation variables and another for camera control variables. Added some preliminary camera movement controls, Added a button to regenerater the lightning pattern. Updated to Glad 4.6

--- LightningOpenGL.cpp
+++ LightningOpenGL.cpp
@@ -1,3 +1,10 @@
+/*
+LightningOpenGL
+Creates a lightning bolt using OpenGL and C++
+Currently uses a simple array of lines that follow on from each other to a random position to create a lightning pattern.
+ImGui is used for the GUI which allows editting of various variables used to genereate the lightning pattern.
+*/
+
 #include <iostream>
 #include <vector>
 #include <math.h>
@@ -7,11 +14,15 @@
 
 #define PI 3.14159265
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>\
 
 using glm::vec3;
 using glm::mat4;
@@ -22,8 +33,8 @@ using glm::lookAt;
 float vectorMagnitude(int x, int y, int z);
 void processInput(GLFWwindow* window);
 
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1000;
+const unsigned int SCR_HEIGHT = 800;
 
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
@@ -166,14 +177,24 @@ vec3 ConvertWorldToScreen(vec3 pos) {
 	return pos;
 }
 
-const int xVariation = 300;
-const int yVariationMax = 160;
-const int yVariationMin = 80;
-const int yMultiplyer = 2;
-const int zVariation = 500;
+// Lightning generation Variables
+int xVariation = 300;
+int yVariationMax = 160;
+int yVariationMin = 80;
+int yMultiplyer = 2;
+int zVariation = 500;
 
-const int numLines = 500;
+int numLines = 500;
+int numLinesMax = 1000;
 
+// Camera controls
+float cameraPosx = 40;
+float cameraPosy = 40;
+float cameraPosz = 40;
+float cameraAngle = 45;
+float cameraMoveSpeed = 0.1f;
+
+// Random int generators for position variation
 std::default_random_engine generator;
 std::uniform_int_distribution<int> x(0, xVariation * 2);
 std::uniform_int_distribution<int> y(yVariationMin, yVariationMax);
@@ -193,25 +214,13 @@ std::shared_ptr<Line> DefineLightningLines(vec3 startPos) {
 
 	vec3 point1 = startPos;
 	vec3 point2 = point1;
-	//int R = 1;
+
 	// make all the lines
 	for (int i = 0; i < numLines; i++) {
 
 		int dx = rollx() - xVariation;
 		int dy = -rolly() * yMultiplyer;
 		int dz = rollz() - zVariation;
-
-
-
-		//int dx = (std::rand() % 2*xVariation) - xVariation;
-		//int dy = -(std::rand() % (yVariationMax - yVariationMin + 1) + yVariationMin);
-		//int dz = (std::rand() % 2*zVariation) - zVariation;
-
-		/*	// Spiral Patern going down
-*
-		point2.x += (float)cos(i) * R;
-		point2.y += -(i * R / 4);
-		point2.z += (float)sin(i) * R;*/
 
 		point2.x += dx;
 		point2.y += dy;
@@ -220,7 +229,7 @@ std::shared_ptr<Line> DefineLightningLines(vec3 startPos) {
 		linesPtr.get()[i].setup(ConvertWorldToScreen(point1), ConvertWorldToScreen(point2));
 		linesPtr.get()[i].setColor(vec3(1, 1, 0));
 		//std::cout << ConvertWorldToScreen(point1).x << "," << ConvertWorldToScreen(point1).y << "," << ConvertWorldToScreen(point1).z << std::endl;
-		std::cout << dx << "  " << dy << " " << dz << std::endl;
+		//std::cout << dx << "  " << dy << " " << dz << std::endl;
 		point1 = point2;
 		// each next line has start equal to the previous
 	}
@@ -242,6 +251,48 @@ float vectorMagnitude(int x, int y, int z)
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	// move camera position
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		cameraPosy += cameraMoveSpeed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		cameraPosy -= cameraMoveSpeed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		cameraPosx -= cameraMoveSpeed;
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		cameraPosx += cameraMoveSpeed;
+	}
+}
+
+void ImGuiWindow(std::shared_ptr<float> rotationSpeed) {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Lightning");
+	// checkboxes
+	//ImGui::Checkbox("Branch", branching);	// TODO: impliment branching
+
+	// Lightning Generation window
+	ImGui::Text("Line generation variables:");
+	ImGui::SliderInt("xVariation", &xVariation, 1, 1000);
+	ImGui::SliderInt("yVariationMax", &yVariationMax, 1, 500);
+	ImGui::SliderInt("yVariationMin", &yVariationMin, 1, 500);
+	ImGui::SliderInt("zVariation", &zVariation, 1, 1000);
+	ImGui::SliderInt("numLines", &numLines, 50, numLinesMax);
+	ImGui::End();
+
+	// Camera Control window
+	ImGui::Begin("Camera");
+	ImGui::Text("Camera options:");
+	ImGui::SliderFloat("rotationSpeed", rotationSpeed.get(), 0.0f, 100.0f);
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 int main() {
@@ -266,46 +317,67 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	camera.position = vec3(40, 40, 40);
-
-	// projection matrix
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+	// camera and projection setup
+	camera.position = vec3(cameraPosx, cameraPosy, cameraPosz);
+	glm::mat4 projection = glm::perspective(glm::radians(cameraAngle), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
 	float angle = 0.0f;
-	float rotationSpeed = 50.0f;
+	// shared pointer, so it can be passed to the imgui window function
+	std::shared_ptr<float> rotationSpeed(new float(0.0f), std::default_delete<float>());
 
 	// draw lines here -------------------------
 	std::shared_ptr<Line> linesPtr(DefineLightningLines(vec3(400, 8000, 0)));
-
 	//------------------------------------------
 
-	// render loop
+	// initialize imgui
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
 
+	// render loop
 	while (!glfwWindowShouldClose(window)) {
+		// set background color
+		glClearColor(0.0, 0.0, 0.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// keep time for rotation speed
 		float currentFrame = (float)glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 
 		// input
 		processInput(window);
 
-		glClearColor(0.0, 0.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
+		// recalculate lines		TODO: move to processInput, had problems with changing the shared pointer in processInput.
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+			std::cout << "New Lightning Lines" << std::endl;
+			linesPtr.reset();
+			linesPtr = DefineLightningLines(vec3(400, 8000, 0));
+		}
 
 		// update camera position(rotating)
-		angle = deltaTime * rotationSpeed;
-		camera.position = vec3(40 * cos(radians(angle)), 40, 40 * sin(radians(angle)));
-
+		angle = deltaTime * *rotationSpeed;
+		camera.position = vec3(cameraPosx * cos(radians(angle)), cameraPosy, cameraPosz * sin(radians(angle)));
 		mat4 view = lookAt(camera.position, vec3(0, 0, 0), vec3(0, 1, 0));
 
+		// draw lines
 		for (int i = 0; i < 100; i++) {
 			linesPtr.get()[i].setMVP(projection * view);
 			linesPtr.get()[i].draw();
 		}
 
+		ImGuiWindow(rotationSpeed);
+
 		// glfw: swap buffers and poll IO events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// imgui: shutdown and cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	//glfw: terminate, clearing all previously allocated GLFW resources.
 	glfwTerminate();
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # LightningOpenGL
 An exploration of the methods used to create realistic looking and behaving lightning
+
+## Uses
+- glad-4.6
+- glfw-3.3.8
+- glm-0.9.9.8
+- imgui-1.89.3
+
+## How to use
+
+Space - Recalculates the lightning pattern.
+
+W/A/S/D - Move the camera
+
+### TODO:
+- clean up and comment code, more modular.
+- add requirements.
+- Move the recalculate lines section to the processInput function.
+- Add mouse control for camera.  
+- Add option to create branches within pattern.
